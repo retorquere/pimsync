64f5313abe32adf947599bca961c86b8bd82fa4f
Hugo Osvaldo Barrera <hugo@whynothugo.nl>
2023-12-22 14:31:19 +0100

    Don't panic on conflict

--- vstorage/src/sync/execute.rs
+++ vstorage/src/sync/execute.rs
@@ -6,7 +6,7 @@
 
 use std::sync::Arc;
 
-use log::debug;
+use log::{debug, error};
 
 use crate::{
     base::{Item, Storage},
@@ -72,7 +72,9 @@ impl ItemAction {
                     )
                     .await?;
                 }
-                Action::Conflict => todo!("conflict resolution"),
+                Action::Conflict => {
+                    error!("Conflict for items {}. Skipping.", self.uid());
+                }
             };
             Ok(())
         }
