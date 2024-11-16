1f961d8e6384b815b791806ec62e684244bc2f43
Hugo Osvaldo Barrera <hugo@whynothugo.nl>
2024-11-16 13:54:25 +0100

    Fix lint in build.rs

--- vdirsyncer/build.rs
+++ vdirsyncer/build.rs
@@ -3,14 +3,14 @@ use std::process::Command;
 fn main() {
     if std::env::var("PIMSYNC_VERSION").is_err() {
         let version = "$Format:%(describe)$"; // Replaced by git-archive.
-        let version = if !version.starts_with('$') {
-            String::from(version)
-        } else {
+        let version = if version.starts_with('$') {
             match Command::new("git").args(["describe", "--tags"]).output() {
                 Ok(o) if o.status.success() => String::from_utf8_lossy(&o.stdout).trim().to_owned(),
                 Ok(o) => panic!("git-describe exited non-zero: {}", o.status),
-                Err(err) => panic!("failed to execute git-describe: {}", err),
+                Err(err) => panic!("failed to execute git-describe: {err}"),
             }
+        } else {
+            String::from(version)
         };
         println!("cargo:rustc-env=PIMSYNC_VERSION={version}");
     }
