4a47ea4e2ed5dfc8e8ac79fb96a02e6ca8233f5e
Hugo Osvaldo Barrera <hugo@whynothugo.nl>
2024-11-27 01:00:14 +0100

    config: reject bogus collection directive
    When one than more parameter is passed, bail. At most one parameter is
    allowed.

--- vdirsyncer/src/config.rs
+++ vdirsyncer/src/config.rs
@@ -262,24 +262,32 @@ fn init_pair<I: Item>(
 }
 
 fn parse_collection_directive(mut directive: Directive) -> anyhow::Result<Collections> {
-    if let Some(param) = directive.params().first() {
-        // TODO: should bail if more than one parameter is specified?
-        let name = param.parse().context("Parsing collection id")?;
-        Ok(Collections::Named(name))
-    } else {
-        let mut child = directive
-            .take_child()
-            .context("Collection directive must specify an id or a block")?;
+    let mut params = directive.take_params();
+    match params.len() {
+        1 => params
+            .remove(0)
+            .parse()
+            .context("Parsing collection id")
+            .map(Collections::Named),
+        2.. => bail!(
+            "unexpected second parameter {} in collection directive",
+            params[1]
+        ),
+        0 => {
+            let mut child = directive
+                .take_child()
+                .context("Collection directive must specify an id or a block")?;
 
-        let alias = take_single_param_from_directive(&mut child, "alias")?;
-        let id_a = take_single_directive(&mut child, "id_a")?;
-        let href_a = take_single_directive(&mut child, "href_a")?;
-        let id_b = take_single_directive(&mut child, "id_b")?;
-        let href_b = take_single_directive(&mut child, "href_b")?;
+            let alias = take_single_param_from_directive(&mut child, "alias")?;
+            let id_a = take_single_directive(&mut child, "id_a")?;
+            let href_a = take_single_directive(&mut child, "href_a")?;
+            let id_b = take_single_directive(&mut child, "id_b")?;
+            let href_b = take_single_directive(&mut child, "href_b")?;
 
-        let a = parse_individual_collection(id_a, href_a)?;
-        let b = parse_individual_collection(id_b, href_b)?;
-        Ok(Collections::Mapped(alias, a, b))
+            let a = parse_individual_collection(id_a, href_a)?;
+            let b = parse_individual_collection(id_b, href_b)?;
+            Ok(Collections::Mapped(alias, a, b))
+        }
     }
 }
 
