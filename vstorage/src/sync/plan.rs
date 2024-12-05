acbc2c100b59673e0e2b0ee11aeec7982f6b1aaa
Hugo Osvaldo Barrera <hugo@whynothugo.nl>
2024-12-05 22:22:18 +0100

    Remove stale TODO
    This was implemented elsewhere.

--- vstorage/src/sync/plan.rs
+++ vstorage/src/sync/plan.rs
@@ -809,7 +809,6 @@ pub enum CollectionAction {
     CreateInB,
     CreateInBoth,
     Delete(MappingUid, Side),
-    // TODO: FlushState (for collections that are gone)
 }
 
 impl CollectionAction {
