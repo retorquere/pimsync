retorquere/pimsync

commit 99ab987256f1094994ada4ec9b09f4371e49ac6c

Enforce foreign keys
Note that this may be disabled when building sqlite. This should not
have any negative side-effects, but is unsupported.

See: https://todo.sr.ht/~whynothugo/pimsync/112

vstorage/src/sync/status.rs
@@ -150,6 +150,7 @@ impl StatusDatabase {
         };
 
         db.init_schema()?;
+        db.conn.execute("PRAGMA foreign_keys = ON")?;
 
         Ok(db)
     }

commit ddde20ff3e61c3d2e7655a471cdae0a5f79a68ea

Fix tests broken due to enforced foreign keys
Fixes: 99ab987256f1094994ada4ec9b09f4371e49ac6c

vstorage/src/sync/status.rs
@@ -581,6 +581,13 @@ mod test {
             href: "work/item.ics".into(),
             etag: "abc000".into(),
         };
+        db.get_or_add_collection(
+            "/collections/work/",
+            "work",
+            Some(&"work".parse().unwrap()),
+            Some(&"work".parse().unwrap()),
+        )
+        .unwrap();
         db.insert_item(mapping_uid, uid, hash, &item_a, &item_b)
             .unwrap();
 
@@ -638,6 +645,13 @@ mod test {
             href: "work/item.ics".into(),
             etag: "abc000".into(),
         };
+        db.get_or_add_collection(
+            "/collections/work/",
+            "work",
+            Some(&"work".parse().unwrap()),
+            Some(&"work".parse().unwrap()),
+        )
+        .unwrap();
         db.insert_item(mapping_uid, uid, hash, &item_a, &item_b)
             .unwrap();
 
@@ -701,6 +715,13 @@ mod test {
             href: "work/item.ics".into(),
             etag: "abc000".into(),
         };
+        db.get_or_add_collection(
+            "/collections/work/",
+            "work",
+            Some(&"work".parse().unwrap()),
+            Some(&"work".parse().unwrap()),
+        )
+        .unwrap();
         db.insert_item(mapping_uid, uid, hash, &item_a, &item_b)
             .unwrap();
 